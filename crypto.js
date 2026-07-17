e34e2703e1b2766760abdccb0da921dfa29ad50a
Vansh <vanshbhatnagar2808@gmail.com>
2026-07-17 10:32:58 +0530

    Update crypto.js

--- crypto.js
+++ crypto.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import store from "./store";
 import { Provider } from "react-redux";
 import Coincreate from "./coincreate";
+import Head from "./Head";
 
 function App(){
     return(
         <Provider store={store}>
+            <Head></Head>
             <Coincreate></Coincreate>
 
         </Provider>
