5393320607052c350802168b021d37bbcc192ad2
Vansh <vanshbhatnagar2808@gmail.com>
2026-07-17 10:31:26 +0530

    Update coincard.js

--- coincard.js
+++ coincard.js
@@ -1,7 +1,7 @@
 export default function Coincard({coin}){
     return(
           <div className="card">
-            <img src={coin.image} alt={coin.name}/>
+            <img className="img" src={coin.image} alt={coin.name}/>
             <h3>{coin.name}</h3>
             <p>price:${coin.current_price}</p>
             <p>market cap rank:{coin.market_cap_rank} </p>
